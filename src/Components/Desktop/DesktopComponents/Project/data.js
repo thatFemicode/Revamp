63d90d980fd216b75777d1aaf0964c4cd0446150
thatFemicode <leyedavid10@gmail.com>
2022-04-18 16:08:02 +0100

    added projects

--- src/Components/Desktop/DesktopComponents/Project/data.js
+++ src/Components/Desktop/DesktopComponents/Project/data.js
@@ -1,28 +1,16 @@
 import git from "./git.svg";
 import Bar from "./Bar.jpg";
-// import Res from "./Res.jpg";
-// import Express from "./Express.png";
 import NBA from "./NBA.png";
 import Tracker from "./Tracker.png";
 import Bmi from "./Bmi.png";
 import Warriors from "./Warriors.png";
+import Women from "./blackwomen.png";
+import Nft from "./nftmarket.png";
 const content = [
-  // {
-  //   id: 1,
-  //   name: "Express Bank",
-  //   body:
-  //     "A fictional minimalist bank app built using array methods, and pure functions, leverages 3rd party api to get location providing weather update and giving time to app users when in use, Webapp follows principles of an SPA. A mini backend for authentication is done but using array methods. Design inspiration by me",
-  //   tools: ["HTML", "SASS", "JS", "GSAP"],
-  //   url: "https://express-bank.netlify.app/",
-  //   git: git,
-  //   image: Express,
-  //   gitUrl: "https://github.com/thatFemicode/Express-bank-app",
-  // },
   {
     id: 1,
     name: "Expense Tracker",
-    body:
-      "Expense tracker used to monitor finances, uses context api for state management, Nodejs(Express) for backend and Mongodb as sql database. Design inspiration by me  ",
+    body: "Expense tracker used to monitor finances, uses context api for state management, Nodejs(Express) for backend and Mongodb as sql database. Design inspiration by me  ",
     tools: [
       "React",
       "Stlyed Components",
@@ -40,8 +28,7 @@ const content = [
   {
     id: 2,
     name: "Baradel Designo",
-    body:
-      "A fictional travel website, design inspiration was gotten from Frontend mentor as a challenge",
+    body: "A fictional travel website, design inspiration was gotten from Frontend mentor as a challenge",
     tools: ["HTML", "SASS", "JS"],
     url: "https://baradeldesigno.netlify.app/",
     git: git,
@@ -51,8 +38,7 @@ const content = [
   {
     id: 3,
     name: "Golden State Warriors ",
-    body:
-      "A Landing page dedicated to the Golden state warriors paying respect to past accomplishment and looking at the future",
+    body: "A Landing page dedicated to the Golden state warriors paying respect to past accomplishment and looking at the future",
     tools: ["React", "Stlyed Components", "GSAP"],
     url: "https://goldenstate.netlify.app/",
     git: git,
@@ -62,8 +48,7 @@ const content = [
   {
     id: 4,
     name: "NBA BLOG APP",
-    body:
-      "Blog website built to connect nba lovers together and discuss about their favorite players, uses Redux for state management, Nodejs(Express) for backend and Mongodb as sql database. Design inspiration by me",
+    body: "Blog website built to connect nba lovers together and discuss about their favorite players, uses Redux for state management, Nodejs(Express) for backend and Mongodb as sql database. Design inspiration by me",
     tools: [
       "React",
       "Stlyed Components",
@@ -81,13 +66,33 @@ const content = [
   {
     id: 5,
     name: "BMI Calculator",
-    body:
-      "A bmi calculator used to calculate body mass index for individuals, uses the metric system for its measurement and local storage to persists data ",
+    body: "A bmi calculator used to calculate body mass index for individuals, uses the metric system for its measurement and local storage to persists data ",
     tools: ["React", "Stlyed Components", "GSAP"],
     url: "https://expressbmicalculator.netlify.app/",
     git: git,
+
     image: Bmi,
     gitUrl: "https://github.com/thatFemicode/BMI-calculator",
+  },
+  {
+    id: 6,
+    name: "Black Women Rule",
+    body: "A website i built to appreciate black women during black history month and women's month ",
+    tools: ["React", "Stlyed Components", "GSAP"],
+    url: "https://blackwomen.netlify.app/",
+    git: git,
+    image: Women,
+    gitUrl: "https://github.com/thatFemicode/Black-Women",
+  },
+  {
+    id: 7,
+    name: "ExpressSea NFT Marketplace",
+    body: "A mini NFT market place built to mimic OpenSea, built this project after learning a bit about web3.0 and nft's as a beginner project. Web app has a landing page and a market place.  ",
+    tools: ["React", "Stlyed Components", "GSAP", "framer-motion"],
+    url: "https://expresssea.netlify.app/",
+    git: git,
+    image: Nft,
+    gitUrl: "https://github.com/thatFemicode/NFT-marketplace",
   },
 ];
 
--- src/Components/Mobile/MobileComponents/Projects/data.js
+++ src/Components/Mobile/MobileComponents/Projects/data.js
@@ -3,8 +3,7 @@ const content = [
   {
     id: 1,
     name: "Expense Tracker",
-    body:
-      "Expense tracker used to monitor finances, uses context api for state management, Nodejs(Express) for backend and Mongodb as sql database. Design inspiration by me  ",
+    body: "Expense tracker used to monitor finances, uses context api for state management, Nodejs(Express) for backend and Mongodb as sql database. Design inspiration by me  ",
     tools: [
       "React",
       "Stlyed Components",
@@ -21,8 +20,7 @@ const content = [
   {
     id: 2,
     name: "Baradel Designo",
-    body:
-      "A fictional travel website, design inspiration was gotten from Frontend mentor as a challenge",
+    body: "A fictional travel website, design inspiration was gotten from Frontend mentor as a challenge",
     tools: ["HTML", "SASS", "JS"],
     url: "https://baradeldesigno.netlify.app/",
     git: git,
@@ -31,8 +29,7 @@ const content = [
   {
     id: 3,
     name: "Golden State Warriors ",
-    body:
-      "A Landing page dedicated to the Golden state warriors paying respect to past accomplishment and looking at the future",
+    body: "A Landing page dedicated to the Golden state warriors paying respect to past accomplishment and looking at the future",
     tools: ["React", "Stlyed Components", "GSAP"],
     url: "https://goldenstate.netlify.app/",
     git: git,
@@ -41,8 +38,7 @@ const content = [
   {
     id: 4,
     name: "NBA BLOG APP",
-    body:
-      "Blog website built to connect nba lovers together and discuss about their favorite players, uses Redux for state management, Nodejs(Express) for backend and Mongodb as sql database. Design inspiration by me",
+    body: "Blog website built to connect nba lovers together and discuss about their favorite players, uses Redux for state management, Nodejs(Express) for backend and Mongodb as sql database. Design inspiration by me",
     tools: [
       "React",
       "Stlyed Components",
@@ -59,12 +55,29 @@ const content = [
   {
     id: 5,
     name: "BMI Calculator",
-    body:
-      "A bmi calculator used to calculate body mass index for individuals, uses the metric system for its measurement and local storage to persists data ",
+    body: "A bmi calculator used to calculate body mass index for individuals, uses the metric system for its measurement and local storage to persists data ",
     tools: ["React", "Stlyed Components", "GSAP"],
     url: "https://expressbmicalculator.netlify.app/",
     git: git,
     gitUrl: "https://github.com/thatFemicode/BMI-calculator",
+  },
+  {
+    id: 6,
+    name: "Black Women Rule",
+    body: "A website i built to appreciate black women during black history month and women's month ",
+    tools: ["React", "Stlyed Components", "GSAP"],
+    url: "https://blackwomen.netlify.app/",
+    git: git,
+    gitUrl: "https://github.com/thatFemicode/Black-Women",
+  },
+  {
+    id: 7,
+    name: "ExpressSea NFT Marketplace",
+    body: "A mini NFT market place built to mimic OpenSea, built this project after learning a bit about web3.0 and nft's as a beginner project. Web app has a landing page and a market place.  ",
+    tools: ["React", "Stlyed Components", "GSAP", "framer-motion"],
+    url: "https://expresssea.netlify.app/",
+    git: git,
+    gitUrl: "https://github.com/thatFemicode/NFT-marketplace",
   },
 ];
 
